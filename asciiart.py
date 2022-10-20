87eba35c630cf963e45142ef3930a1a730adb6e7
Renny <100791149+I4M4N4N0DE@users.noreply.github.com>
2022-10-20 16:33:47 +0200

    Update asciiart.py

--- asciiart.py
+++ asciiart.py
@@ -1,23 +1,24 @@
-from art import * # download Python Art library for having the program functional
+import art as ar # download Python Art library for having the program functional
 import time
 
 class Main: # the Main class of the code storing all the methods
     
     def asciiConsole(): # the non-saving generator
-    
-        # generating function
-        print("non-saving mode")
-        print("")
-        text = str(input("Enter the text you want to convert: "))
-        print("Your text is gonna be converted into an ASCII art.")
-        artpiece = text2art(text)
-        print(artpiece)
         
-        # info and restart
-        print("Your art is ready for copying, but ISN'T saved!!!")
-        print("Cancelling the program will erase your art.")
-        print("")
-        Main.asciiConsole()
+        while True:
+            
+            # generating function
+            print("non-saving mode")
+            print("")
+            text = str(input("Enter the text you want to convert: "))
+            print("Your text is gonna be converted into an ASCII art.")
+            artpiece = ar.text2art(text)
+            print(artpiece)
+        
+            # info and restart
+            print("Your art is ready for copying, but ISN'T saved!!!")
+            print("Cancelling the program will erase your art.")
+            print("")
 
     def asciiConsoleSaver(): # the saving generator
        
@@ -26,7 +27,7 @@ class Main: # the Main class of the code storing all the methods
         print("")
         text = str(input("Enter the text you want to convert: "))
         print("Your text is gonna be converted into an ASCII art.")
-        artpiece = text2art(text)
+        artpiece = ar.text2art(text)
         print(artpiece)
         
         # saving the art into a file
@@ -35,9 +36,9 @@ class Main: # the Main class of the code storing all the methods
         print("Your art is now saved.")
         print("")
 
-        # restart
-        def runner():
-    
+        while True:
+            
+            # restart
             print("Restart the program?")
             choice = str(input("[Y/N]: "))
     
@@ -47,27 +48,27 @@ class Main: # the Main class of the code storing all the methods
                 quit()
             else:
                 print("Sorry - didn't understand - again? ")
-                runner()
     
     def main(): # beginning navigator function
-        
+            
         # an introduction
-        tprint("ASCII Art generator")
+        ar.tprint("ASCII Art generator")
         print("SHINY PIECE OF A CODE WHICH TURNS YOUR TEXT INTO AN ASCII ART")
         print("Made with love by Rene Valenta, 2022")
         print("")
         print("Choose the mode you wanna start: ")
         print("")
-        print("[0] ASCII art only into the console (for late copying)")
+        print("[0] ASCII art only into the console (for late copying - repeats until the program is shut down)")
+        
+        while True:
 
-        startup = input("[1] ASCII art that will also save itself into a file automatically: ") 
+            startup = input("[1] ASCII art that will also save itself into a file automatically: ") 
     
-        if startup == "1":
-            Main.asciiConsoleSaver()
-        elif startup == "0":
-            Main.asciiConsole()
-        else:
-            print("Sorry - again?")
-            Main.main()
+            if startup == "1":
+                Main.asciiConsoleSaver()
+            elif startup == "0":
+                Main.asciiConsole()
+            else:
+                print("Sorry - again?")
 
 Main.main()
